d1c4f88f47fe7f9b3e95d751eb9bf8c24f1d913b
El tío judai <alexnarcista@gmail.com>
2026-05-06 21:19:48 -0600

    Refactor smsg function for improved message handling

--- engine/simple.js
+++ engine/simple.js
@@ -1,33 +1,26 @@
-import { getContentType, jidDecode } from '@whiskeysockets/baileys'
-
 export function smsg(conn, m) {
+
     if (!m) return m
-    if (m.key) {
-        m.id = m.key.id
-        m.chat = m.key.remoteJid
-        m.fromMe = m.key.fromMe
-        m.isGroup = m.chat.endsWith('@g.us')
-        m.sender = conn.decodeJid(m.fromMe && conn.user.id || m.participant || m.key.participant || m.chat || '')
-    }
-    if (m.message) {
-        m.mtype = getContentType(m.message)
-        m.msg = (m.mtype == 'viewOnceMessage' ? m.message[m.mtype].message[getContentType(m.message[m.mtype].message)] : m.message[m.mtype])
-        m.body = m.message.conversation || m.msg.caption || m.msg.text || (m.mtype == 'listResponseMessage') && m.msg.singleSelectReply.selectedRowId || (m.mtype == 'buttonsResponseMessage') && m.msg.selectedButtonId || m.text || ''
-    }
-    
-    // Función para responder directo: m.reply('hola')
-    m.reply = (text, chatId, options) => conn.sendMessage(chatId ? chatId : m.chat, { text: text }, { quoted: m, ...options })
-    
+
+    m.chat = m.key.remoteJid
+
+    m.fromMe = m.key.fromMe
+
+    m.sender =
+        m.key.participant ||
+        m.key.remoteJid
+
+    m.isGroup =
+        m.chat.endsWith('@g.us')
+
+    m.body =
+        m.message?.conversation ||
+        m.message?.extendedTextMessage?.text ||
+        m.message?.imageMessage?.caption ||
+        m.message?.videoMessage?.caption ||
+        ''
+
+    m.name = m.pushName || 'Sin nombre'
+
     return m
 }
-
-export function decodeJid(jid) {
-    if (!jid) return jid
-    if (/:\d+@/gi.test(jid)) {
-        let decode = jidDecode(jid) || {}
-        return decode.user && decode.server && decode.user + '@' + decode.server || jid
-    }
-    return jid
-}
-
-export default { smsg, decodeJid }
